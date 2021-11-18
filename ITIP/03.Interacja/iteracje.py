4e8134908a2b82369a59b44a9b0ac9286e1aa024
Jerzy Jasonek <jasonj2333@gmail.com>
2021-11-18 13:25:24 +0100

    add Iteracja lesson

---
+++ ITIP/03.Interacja/iteracje.py
@@ -0,0 +1,20 @@
+names = ['Tomek', 'Ala', 'Zosia', 'Sławek', 'Kasia']
+text = 'Bardzo lubię programować'
+
+
+for name in names:
+    print(names)
+
+print('-'*10)
+
+for i in range(10):
+    print(i)
+
+print('-'*10)
+    
+def print_item(list):
+    for item in list:
+        print(item)
+        
+print_item(text)
+        
---
+++ ITIP/03.Interacja/iteracje_ogolnie.py
@@ -0,0 +1,7 @@
+for element in sekwencja:
+    #Blok kodu do wykonania
+    #poszczegolne elementy sekwencji dostepne pod etykietą element
+    
+    break #opcjonalne przerwanie
+
+    continue #opcjonalne przejście do kolejnego elementu sekwencji
