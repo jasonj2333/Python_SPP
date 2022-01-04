4afbbc8eecc7397d338a4409c7469a532995cbc8
Jerzy Jasonek <jasonj2333@gmail.com>
2022-01-04 09:27:33 +0100

    update lesson 00.TypyDanych

--- ITIP/00.TypyDanych/main.py
+++ ITIP/00.TypyDanych/main.py
@@ -1,10 +1,33 @@
 from mylib import variable_type
 
-tekst = {
+#liczba całkowita - int
+liczba_int = 5
+
+#liczba zmiennoprzecinkowa - float
+liczba_float = 2.78
+
+#ciąg znaków - string (str)
+tekst = "To jest tekst"
+
+#zmienna logiczna - bool (True/False)
+koniec = True
+
+#krotka - tuple
+krotka = (128, 67, 89) #niezmienna lista - struktura indeksowana np. krotka[2] - indeksujemy od 0
+
+#lista - list - struktura indeksowana np. lista[2] - indeksujemy od 0
+lista = [1, 7, "Tomek"] #może zawierać elementy różnych typów
+
+#slownik - dict
+slownik = {
         'Niepolomice' : 16,
         'Krakow' : 19
     }
 
+#zbiór - set
+zbior = {1, 12, "Tomek"}
+#w zbiorze nie ma duplikatów (dwóch takich samych elementów), nie ma też kolejności elementów
+
 #print(type(liczba))
 
-variable_type(tekst)
+variable_type(liczba_float)
