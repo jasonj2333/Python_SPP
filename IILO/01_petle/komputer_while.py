6f0c32cffcf2c5fefb257c7e15e633053f85769e
Jerzy Jasonek <48927964+jasonj2333@users.noreply.github.com>
2023-02-24 11:28:47 +0100

    Delete komputer_while.py

--- IILO/01_petle/komputer_while.py
+++
@@ -1,9 +0,0 @@
-kwota = int(input("Podaj cenę komputera: "))
-
-suma_wplat = 0
-
-while suma_wplat < kwota:
-    wplata = int(input("Podaj wysokość wpłaty: "))
-    suma_wplat = suma_wplat + wplata
-
-print(f"Gratulacje, zebrałeś odpowiednią kwotę, masz: {suma_wplat}")
