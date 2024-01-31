e517727fd03f1eef63a65433cdc4004f046211f1
Jerzy Jasonek <jasonj2333@gmail.com>
2024-01-31 16:06:10 +0100

    update krowy

--- Modelowanie/KrowyZosi/krowy.py
+++ Modelowanie/KrowyZosi/krowy.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 #krowy
 def C(t):
     return 30 * 2**t
@@ -10,7 +12,13 @@ def F(t):
 def H(t):
     return C(t) * F(t)
 
-t = 5
-print(H(t))
+lata = [0,2,4,6,8,10,12]
+siano = []
+#print(H(t))
 #print(240 * 3**t)
+for rok in lata:
+    print(f"Siano konieczne do karmienia {C(rok)} krów wynosi {H(rok):,.2f}")
+    siano.append(H(rok))
 
+plt.plot(lata, siano)
+plt.show()
